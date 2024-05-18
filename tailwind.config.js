3c3d10685f7483b1dcfdd26a77a6d48f8cb9e282
Hardyguardy <fighter2007max@gmail.com>
2024-05-18 16:28:21 +0200

    wip: create structure

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black: {
+          DEFAULT: "#000000",
+          100: "#000319",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
