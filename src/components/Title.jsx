332bcdd0e15fb0d4c36720e82c45132dcbe3c391
Vlad Gerasimovich <blueambr@icloud.com>
2024-06-05 18:15:44 +0200

    fix: code review

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,17 +1,13 @@
 "use client";
 
 import clsx from "clsx";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const Title = ({ children }) => {
   const [isSticky, setIsSticky] = useState(false);
 
   useEffect(() => {
-    window.onscroll = function () {
-      scrollFunction();
-    };
-
-    function scrollFunction() {
+    const scrollFunction = () => {
       if (
         document.body.scrollTop > 150 ||
         document.documentElement.scrollTop > 150
@@ -20,21 +16,21 @@ const Title = ({ children }) => {
       } else {
         setIsSticky(false);
       }
-    }
+    };
+
+    window.onscroll = () => scrollFunction();
   }, []);
+
   return (
     <div
       className={clsx(
-        isSticky ? "justify-center bg-primary " : "",
-        "sticky top-0 z-10 flex w-full transition-all  ",
-        ""
+        isSticky &&
+          "justify-center -translate-x-6 w-[calc(100%+48px)] top-0 py-2 bg-primary sticky z-10",
+        "flex transition-all"
       )}
     >
       <h1
-        className={clsx(
-          isSticky ? "text-white -translate-y-5 " : "text-white",
-          " text-3xl font-bold pt-10 pl-6"
-        )}
+        className={clsx(isSticky && "text-lg", "text-3xl font-bold text-white")}
       >
         {children}
       </h1>
